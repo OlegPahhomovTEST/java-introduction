e1a0d933c57b74b106263269090a50cb8d89cdc5
test.oleg.pahh <oleg2.pahhomov@gmail.com>
2019-02-11 09:29:35 +0200

    I am java terrorist

--- src/main/java/ee/taltech/itcolledge/FirstContact.java
+++ src/main/java/ee/taltech/itcolledge/FirstContact.java
@@ -3,6 +3,7 @@ package ee.taltech.itcolledge;
 public class FirstContact {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Goodbye world!");
+        System.out.println("Don't learn java, learn C");
     }
 }
